4030bc550f120b497be1db45d957deb878e0c56a
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-06-07 16:21:28 +0530

    Updated Sentry

--- pages/_app.js
+++ pages/_app.js
@@ -7,27 +7,18 @@ function MyApp({ Component, pageProps }) {
     <Layout>
       <Script
         id="sentry"
+        src="https://browser.sentry-cdn.com/7.0.0/bundle.tracing.min.js"
+        crossOrigin="anonymous"
+        integrity="sha384-+zViWRWnRAkk9/+V2CRRVm1tuQEGGqye3jiEC8SDdjaOyzmv86+kvpl6NnRy9QIF"
+        priority="true"
         strategy="worker"
-        dangerouslySetInnerHTML={{
-          __html: `(function() { 
-          var sentry = document.createElement('script'); 
-          sentry.async = true; 
-          sentry.src = "https://browser.sentry-cdn.com/7.0.0/bundle.tracing.min.js";
-          sentry.integrity="sha384-+zViWRWnRAkk9/+V2CRRVm1tuQEGGqye3jiEC8SDdjaOyzmv86+kvpl6NnRy9QIF";
-          sentry.crossOrigin="anonymous"; 
-          var s = document.getElementsByTagName('script')[0]; 
-          s.parentNode.insertBefore(sentry, s); 
-        })();`,
-        }}
       />
       <Script id="sentry-init" strategy='worker' dangerouslySetInnerHTML={{
-        __html: `(function() { 
-          Sentry.init({
+        __html: `Sentry.init({
             dsn:"https://e31f798306f746fab3b4fc9b33cbdf0d@o1278195.ingest.sentry.io/6477256",
             integrations: [new Sentry.Integrations.BrowserTracing()],
 		        tracesSampleRate: 1.0
-          });
-          })();`,
+          });`,
       }}>
       </Script>
       <Script
