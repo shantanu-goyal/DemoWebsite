159666556d861ac3d6ea4762f6c6cba99b69cefa
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-06-08 14:47:56 +0530

    Added Sentry CDN

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import Layout from '../components/Layout'
 import Script from 'next/script'
 import '../styles/globals.css'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  const [available,serAvailable]=useState(false);
+  function loadHandler(){
+    serAvailable(true)
+  };
+
   return (
     <Layout>
       <Script
@@ -11,63 +17,19 @@ function MyApp({ Component, pageProps }) {
         crossOrigin="anonymous"
         integrity="sha384-+zViWRWnRAkk9/+V2CRRVm1tuQEGGqye3jiEC8SDdjaOyzmv86+kvpl6NnRy9QIF"
         priority="true"
-        strategy="worker"
+        onLoad={loadHandler}
       />
-      <Script
-        id="sentry-console-capture"
-        src="https://browser.sentry-cdn.com/7.0.0/captureconsole.min.js"
-        crossOrigin="anonymous"
-        integrity="sha384-F9eVzZTC8N8+p6mvSqBoTIuFHbKq2XFPn6ZtNKPkUBslMACSFOHy3/1XkET00hnC"
-        priority="true"
-        strategy="worker"
-      />
-
-      <Script
-        id="sentry-debugging"
-        src="https://browser.sentry-cdn.com/7.0.0/debug.min.js"
-        crossOrigin="anonymous"
-        integrity="sha384-gfwqW2T9/pJzXG5kGMjh219m8NxYr4sFdNEmk8Kcmm03LLR3e7u0MUCJtF+BvwlA
-        "
-        priority="true"
-        strategy="worker"
-      />
-      
-      <Script
-        id="sentry-session-timing"
-        src="https://browser.sentry-cdn.com/7.0.0/sessiontiming.min.js"
-        crossOrigin="anonymous"
-        integrity="sha384-yy0Jah9kyZt8VCnKVBxDj92pIC/E9XFkw5G6NXx2+0ZJbTBn83Y3X32KqAZxhF9b"
-        priority="true"
-        strategy="worker"
-      />
-
-      <Script
-        id="sentry-transaction"
-        src="https://browser.sentry-cdn.com/7.0.0/transaction.min.js"
-        crossOrigin="anonymous"
-        integrity="sha384-p+n3NUtytg2Zgunyx5gQNdbZk7AieZyquN8nOaL7+hbqbe0SZomZIQJ5apsiXuLd"
-        priority="true"
-        strategy="worker"
-      />
-
-      <Script id="sentry-init" strategy='worker' dangerouslySetInnerHTML={{
+      {available && (<Script id="sentry-init"
+        dangerouslySetInnerHTML={{
         __html: `Sentry.init({
             dsn:"https://e31f798306f746fab3b4fc9b33cbdf0d@o1278195.ingest.sentry.io/6477256",
-            integrations: [new Sentry.Integrations.BrowserTracing(), new Sentry.Integrations.Debug(
-              {
-                debugger: boolean,
-                stringify: boolean
-              }
-            ), new Sentry.Integrations.CaptureConsole(
-              {levels: string}
-            ), new Sentry.Integrations.SessionTiming(), new Sentry.Integrations.Transaction() ],
+            integrations: [new Sentry.Integrations.BrowserTracing()],
 		        tracesSampleRate: 1.0
           });`,
       }}>
-      </Script>
+      </Script>)}
       <Script
         id="walkme"
-        strategy="worker"
         dangerouslySetInnerHTML={{
           __html: `(function() { 
           var walkme = document.createElement('script'); 
