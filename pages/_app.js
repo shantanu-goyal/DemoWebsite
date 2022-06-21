dbf85fa2e774eea58bbc41a1dcdb7e6bf2797cf6
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-06-21 13:49:58 +0530

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -28,6 +28,22 @@ function MyApp({ Component, pageProps }) {
           });`,
       }}>
       </Script>)}
+      <Script
+          id="walkme"
+          strategy="lazyOnload"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function() { 
+                var walkme = document.createElement('script'); 
+                walkme.type = 'text/javascript'; 
+                walkme.async = true; 
+                walkme.src = "https://cdn.walkme.com/users/c820617fb6f145dcba1a92a407382865/test/walkme_c820617fb6f145dcba1a92a407382865_https.js"; 
+                var s = document.getElementsByTagName('script')[0]; 
+                s.parentNode.insertBefore(walkme, s); 
+                window._walkmeConfig = {smartLoad:true}; 
+              })();`,
+          }}
+        />
       <Component {...pageProps} />
     </Layout>
   )
