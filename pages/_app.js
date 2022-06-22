1dfc52145aa90834d3be45967a72ed3d5e9b5bdb
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-06-22 17:06:45 +0530

    Updated Beamer

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,8 @@ import '../styles/globals.css'
 import { useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  const [available,serAvailable]=useState(false);
-  function loadHandler(){
+  const [available, serAvailable] = useState(false);
+  function loadHandler() {
     serAvailable(true)
   };
 
@@ -21,18 +21,18 @@ function MyApp({ Component, pageProps }) {
       />
       {available && (<Script id="sentry-init"
         dangerouslySetInnerHTML={{
-        __html: `Sentry.init({
+          __html: `Sentry.init({
             dsn:"https://e31f798306f746fab3b4fc9b33cbdf0d@o1278195.ingest.sentry.io/6477256",
             integrations: [new Sentry.Integrations.BrowserTracing()],
 		        tracesSampleRate: 1.0
           });`,
-      }}>
+        }}>
       </Script>)}
       <Script
-          id="walkme"
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `(function() { 
+        id="walkme"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `(function() { 
                 var walkme = document.createElement('script'); 
                 walkme.type = 'text/javascript'; 
                 walkme.async = true; 
@@ -41,8 +41,16 @@ function MyApp({ Component, pageProps }) {
                 s.parentNode.insertBefore(walkme, s); 
                 window._walkmeConfig = {smartLoad:true}; 
               })();`,
-          }}
-        />
+        }}
+      />
+      <Script id="beamer" type='text/javascript' src="https://app.getbeamer.com/js/beamer-embed.js" />
+       <Script id="beamer-config" dangerouslySetInnerHTML={
+        {
+          __html:`var beamer_config = {
+            product_id : 'uJgeHXDh45550'
+          };`
+        }
+       } />
       <Component {...pageProps} />
     </Layout>
   )
