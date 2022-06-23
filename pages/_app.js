3d00ba4023577fee41b43721656044732ea9cf24
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-06-23 20:42:03 +0530

    Added Hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -61,12 +61,23 @@ function MyApp({ Component, pageProps }) {
         `
         }} />
         <Script id="google-ads" src="https://www.googletagmanager.com/gtag/js?id=UA-232776594-1" />
-        <Script dangerouslySetInnerHTML={{
+        <Script id="google-ads-2" dangerouslySetInnerHTML={{
           __html:`window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'UA-232776594-1');`
         }} />
+        <Script id="hotjar" dangerouslySetInnerHTML={{
+          __html:`(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3034966,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+        }}>
+        </Script>
       <Component {...pageProps} />
     </Layout>
   )
