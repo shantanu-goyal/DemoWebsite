592f1855bc13f9e029bba17524b64d575effba90
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-06-23 21:04:31 +0530

    Added Jquery

--- pages/_app.js
+++ pages/_app.js
@@ -87,6 +87,7 @@ function MyApp({ Component, pageProps }) {
         'Last purchase date': '2019-06-20Z'
         });`
       }} />
+      <Script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous" />
       <Component {...pageProps} />
     </Layout>
   )
